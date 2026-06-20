698f95d2610bc53797a70886e5465abed17f15ff
Lethabo Mafihle James Moshabane <Getintouchwithlethabo@gmail.com>
2026-06-20 13:27:18 +0200

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
+from analyzer_service import analyze_resume
+from parser import extract_pdf_text
+from models import AnalyzeRequest
 
-app = FastAPI()
+app = FastAPI(
+    title="AI Resume Analyzer API",
+    description="AI-powered resume analyzer built with Python and FastAPI.",
+    version="1.0.0",
+)
+
 
 @app.get("/")
 def home():
     return {
-        "message": "AI Resume Analyzer API"
+        "message": "AI Resume Analyzer API is running"
+    }
+
+
+@app.post("/analyze")
+def analyze_text(request: AnalyzeRequest):
+    result = analyze_resume(
+        request.resume_text,
+        request.job_text
+    )
+
+    return result
+
+
+@app.post("/analyze-resume")
+async def analyze_uploaded_resume(
+    file: UploadFile = File(...),
+    job_text: str = Form(...)
+):
+    file_content = await file.read()
+
+    resume_text = extract_pdf_text(file_content)
+
+    result = analyze_resume(
+        resume_text,
+        job_text
+    )
+
+    return {
+        "filename": file.filename,
+        "analysis": result
     }
